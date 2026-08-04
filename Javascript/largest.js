6628464fd8094e1914cc52e59d04893c782cf7ab
Ritik Pal <ritikpal5437@gmail.com>
2026-08-04 22:54:49 +0530

    Add largest.js to implement logic for determining the largest of three numbers

---
+++ Javascript/largest.js
@@ -0,0 +1,13 @@
+let a = 100;
+let b = 2;
+let c = 54;
+if ( a > b && a > c){
+    console.log(" a is largest");
+}
+else if ( b > a && b> c ){
+    console.log(" b is largest");
+}
+else{
+    console .log(" c is largest")
+}
+
