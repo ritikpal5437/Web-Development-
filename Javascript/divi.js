3f17ea52002e1555b5a2a95ac049db8da9a99655
Ritik Pal <ritikpal5437@gmail.com>
2026-08-04 23:07:46 +0530

    Add divi.js and grade.js to implement divisibility and grading logic

---
+++ Javascript/divi.js
@@ -0,0 +1,9 @@
+let n=30;
+if( n%3==0){
+    console.log(" divisible by 3");
+}else if( n%5==0){
+    console.log(" divisible by 5");
+}
+else{
+    console.log(" not divisible by 3 & 5")
+}
---
+++ Javascript/grade.js
@@ -0,0 +1,22 @@
+let g = 00;
+if ( g >= 90){
+    console.log("A grade");
+}
+else if (g>= 80){
+    console.log("B grade")
+
+}
+else if (g>= 70){
+    console.log("C grade")
+
+}
+else if (g>= 60){
+    console.log("D grade")
+
+}
+else if (g>= 50){
+    console.log("E grade")
+
+}else {
+    console.log("fail")
+}
