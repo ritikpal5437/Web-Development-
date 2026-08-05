1f9dd2c0bbd7d7112540ffce52624bd74510411f
Ritik Pal <ritikpal5437@gmail.com>
2026-08-05 23:04:34 +0530

    funtion

--- Javascript/func.js
+++ Javascript/func.js
@@ -6,6 +6,16 @@ function sqrt(n){
     return n*n;
     
 }
+function iseven(n){
+    if ( n %2==0){
+        return("even");
+    }
+    else{
+        return odd;
+    }
+
+
+}
  
 let result = sqrt(5);
 console.log(result);
@@ -13,3 +23,45 @@ console.log(result);
 
 let result1 = add(5,10);
 console.log(result1);
+
+let result3 = iseven(5);
+console.log(result3);
+
+ 
+
+
+function largest(a,b,c ){
+    if( a>b && a>c){
+        return a;
+    }
+    else if ( b > a && b > c)
+        {
+        return b;
+    }
+    else{
+        return c;
+    }
+}
+
+let result= largest(5555,666666,555555555);
+console.log( "The largest No. is "+result);
+
+
+
+const add = function(a, b) {
+    return a + b;
+};
+
+console.log(add(10, 30));
+const add = (a, b) => a + b;
+
+console.log(add(5, 10));
+
+
+const cube= function(n){
+    return n*n*n;
+}
+console.log(cube(3));
+
+const multi= (a,b)=> a*b;
+ console.log(multi(5,10));
