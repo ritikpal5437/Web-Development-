839cd4040f4f28fb855fd607e797eb5e7e53acdb
Ritik Pal <ritikpal5437@gmail.com>
2026-08-05 22:02:26 +0530

    Refactor numbers function to print even numbers from 1 to 10

--- Javascript/no.js
+++ Javascript/no.js
@@ -1,7 +1,13 @@
-function numbers(){
-    for ( let i = 0; i<=5;i++)
-        console.log(i)
+function numbers() {
+    for (let i = 1; i <= 10; i++) {
+        if (i % 2 == 0) {
+            console.log(i)
+        }
+
+    }
 }
+
+
 
 
 
