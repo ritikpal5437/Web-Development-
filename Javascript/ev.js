6cf000d85040a4810a26b0f2123eeb16bd03e80d
Ritik Pal <ritikpal5437@gmail.com>
2026-08-05 13:11:18 +0530

    Add multiple JavaScript files for various functionalities including factorial calculation, sum of digits, and counting even numbers

---
+++ Javascript/ev.js
@@ -0,0 +1,9 @@
+let count = 0;
+
+for( let i = 1; i <= 100; i++){
+    if(i % 2 == 0) {
+        // console.log(i);
+        count++;
+    }
+}
+console.log("Count of even numbers: " + count);
--- Javascript/tempCodeRunnerFile.js
+++ Javascript/tempCodeRunnerFile.js
@@ -1,3 +1,1 @@
-for(let i = 1; i <= 10; i++) {
-    console.log(i*7);
-}
+ // console.log(i);
