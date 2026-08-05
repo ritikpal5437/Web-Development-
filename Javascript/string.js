1e1b79c8844d66cbb5c47aafab8a21a369664367
Ritik Pal <ritikpal5437@gmail.com>
2026-08-05 23:20:21 +0530

    Add iseven function to check for even numbers; refactor existing functions for clarity

---
+++ Javascript/string.js
@@ -0,0 +1,24 @@
+// let name = "JavaScript";
+
+// console.log(name.length);
+// console.log(name[0]);
+// console.log(name[name.length-1]);
+
+// for(let i =0;i<name.length;i++){
+//     console.log(name[i]);
+// }
+
+let first = "ritik";
+let sec  = "pal";
+console.log(first+" "+sec);
+
+
+let name = "ritik";
+console.log(`my name is ${name}`);
+
+
+
+let str = "   hello worlD    ";
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str.trim());
