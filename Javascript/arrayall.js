6501e916127b3833cbf48d4f290c1040d8c7a9a0
Ritik Pal <ritikpal5437@gmail.com>
2026-08-07 12:14:38 +0530

    Add arrayall.js file with initial array declarations and console logs

---
+++ Javascript/arrayall.js
@@ -0,0 +1,31 @@
+// let arr =[ 44, 55, 66, 77, 88, 99];
+// console.log(arr.length);
+// console.log(arr[0]);
+// console.log(arr[arr.length - 1]); 
+// for( let i =0 ; i<arr.length;i++){
+//     console.log(arr[i]);
+// }
+
+let colors= ["Red",'Blue',"Green"];
+console.log(colors[0]);
+console.log(colors[1]);
+console.log(colors[2]);
+
+
+let num=[ 10,20,30,40,50];
+console.log(num.length);
+
+let fruit=["apple","banana","mango"];
+fruit[1]="orange";
+console.log(fruit);
+
+
+let city=["delhi","mumbai","pune","chennai"];
+for(let i = 0;i<city.length;i++){
+    console.log(city[i]);
+}
+
+let marks = [80, 90, 70, 60, 95];
+for(let i =0;i<marks.length;i++){
+    console.log(marks[i]);
+}
