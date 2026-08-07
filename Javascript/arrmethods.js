de34b9c0b37d17e7f24036852eee62bb536a47f3
Ritik Pal <ritikpal5437@gmail.com>
2026-08-07 12:24:57 +0530

    Add arrmethods.js file with array manipulation methods and console logs

---
+++ Javascript/arrmethods.js
@@ -0,0 +1,26 @@
+// push()     End me add
+// pop()      End se remove
+
+// unshift()  Start me add
+// shift()    Start se remove
+
+let arr=[1,2,3];
+arr.push(4);
+console.log(arr);
+
+
+
+let arr1=[10,20,30,40];
+arr1.pop();
+console.log(  arr1);
+
+
+let arr2=[20,30,40];
+arr2.unshift(10);
+console.log(  arr2);
+
+
+let arr3=[10,20,30,40];
+arr3.shift();
+console.log(  arr3);
+
