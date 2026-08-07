021256a2653764150884bbbdad1f73b96e1a8888
Ritik Pal <ritikpal5437@gmail.com>
2026-08-07 22:24:27 +0530

    Add initial HTML and JavaScript files for DOM manipulation example

---
+++ Javascript/Dom.js
@@ -0,0 +1,7 @@
+// // console.log(document);
+// let heading = document.getElementById("title");
+
+// console.log(title);
+let title = document.getElementById("title");
+
+title.innerText = "Frontend Development";
