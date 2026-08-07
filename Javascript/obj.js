20a48251d269380a095ac3b12c7bfbb7e607feb6
Ritik Pal <ritikpal5437@gmail.com>
2026-08-07 17:15:03 +0530

    Add obj.js file with student object and console logs for properties

---
+++ Javascript/obj.js
@@ -0,0 +1,17 @@
+let student = {
+    name: "Ritik",
+    age: 22,
+    city: "Haridwar"
+};
+
+console.log(student.name);
+console.log(student.age);
+console.log(student.city);
+
+student.age = 23;
+
+console.log(student);
+
+student.country = "India";
+
+console.log(student);
