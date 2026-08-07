f5ffa49ecff59c90a3accfe773c789d533d6dbcd
Ritik Pal <ritikpal5437@gmail.com>
2026-08-07 22:33:59 +0530

    Refactor DOM structure and enhance JavaScript logging for better debugging

--- Javascript/Dom.js
+++ Javascript/Dom.js
@@ -5,3 +5,9 @@
 let title = document.getElementById("title");
 
 title.innerText = "Frontend Development";
+console.log(document.body.children);
+console.log(document.body);
+console.log(document.body.children);
+console.log(document.getElementById("box").children);
+console.log(document.getElementById("box").firstElementChild);
+console.log(document.getElementById("box").lastElementChild);
