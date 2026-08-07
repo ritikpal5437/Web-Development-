8b3e2622ee3fc2808e490061030e75c166685369
Ritik Pal <ritikpal5437@gmail.com>
2026-08-07 12:34:16 +0530

    Add moremethods.js file with array examples and console logs

---
+++ Javascript/moremethods.js
@@ -0,0 +1,12 @@
+// let arr = [10, 20, 30];
+// console.log(arr.includes(20));
+
+let arr = ["HTML", "CSS", "JavaScript"];
+console.log(arr.indexOf("CSS"));
+
+
+let arr1 = ["Apple", "Banana", "Mango"];
+console.log(arr1.join("|"));
+
+let arr2 = [5, 10, 15, 20];
+console.log(arr2.reverse());
