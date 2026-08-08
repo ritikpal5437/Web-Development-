807dcabb3d131d01f7c33357c205536effb9493e
Ritik Pal <ritikpal5437@gmail.com>
2026-08-08 17:07:31 +0530

    Add initial HTML and JavaScript files for DOM manipulation

---
+++ Javascript/script.js
@@ -0,0 +1,12 @@
+let heading = document.getElementById("heading");
+console.log(heading);
+
+heading.innerText = "Hello Ritik";
+
+let para = document.getElementsByClassName("text");
+
+console.log(para);
+console.log(para[0]);
+console.log(para[1]);
+
+para[0].innerText = "First Paragraph";
