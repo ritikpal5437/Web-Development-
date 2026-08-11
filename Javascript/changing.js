789b9dc8912cec6353496c4b13e23f3b358c13e1
Ritik Pal <ritikpal5437@gmail.com>
2026-08-11 16:01:42 +0530

    Update DOMtag.html and changing.js to add new elements and enhance content

--- Javascript/changing.js
+++ Javascript/changing.js
@@ -1,11 +1,12 @@
-// let heading = document.getElementById("heading");
-// heading.innerHTML = "Welcome to the New Page!";
-
-
-// // let heading = document.getElementById("heading");
-
-// // heading.innerHTML = "<span>Hello DOM</span>";
-
 let title = document.getElementById("title");
+let message = document.getElementById("message");
+let box = document.getElementById("box");
 
 title.innerText = "JavaScript DOM";
+
+message.innerHTML = "<b>Hello DOM</b>";
+
+box.innerHTML = `
+    <h2>Welcome</h2>
+    <p>Learning JS</p>
+`;
