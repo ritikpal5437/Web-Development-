17ad95efb093554790e8db68fc84a03788afdac4
Ritik Pal <ritikpal5437@gmail.com>
2026-08-11 15:47:38 +0530

    Refactor DOMtag.html and changing.js to comment out unused code and update title text

--- Javascript/changing.js
+++ Javascript/changing.js
@@ -1,7 +1,11 @@
-let heading = document.getElementById("heading");
-heading.innerHTML = "Welcome to the New Page!";
-
-
 // let heading = document.getElementById("heading");
+// heading.innerHTML = "Welcome to the New Page!";
 
-// heading.innerHTML = "<span>Hello DOM</span>";
+
+// // let heading = document.getElementById("heading");
+
+// // heading.innerHTML = "<span>Hello DOM</span>";
+
+let title = document.getElementById("title");
+
+title.innerText = "JavaScript DOM";
