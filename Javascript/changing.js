8dad4aaa2cd8d8b9d2601e382ddc6496f19d8c55
Ritik Pal <ritikpal5437@gmail.com>
2026-08-11 16:14:35 +0530

    Refactor DOMtag.html and changing.js to comment out unused code and clean up structure

--- Javascript/changing.js
+++ Javascript/changing.js
@@ -1,12 +1,19 @@
+// let title = document.getElementById("title");
+// let message = document.getElementById("message");
+// let box = document.getElementById("box");
+
+// title.innerText = "JavaScript DOM";
+
+// message.innerHTML = "<b>Hello DOM</b>";
+
+// box.innerHTML = `
+//     <h2>Welcome</h2>
+//     <p>Learning JS</p>
+// `;
+
 let title = document.getElementById("title");
-let message = document.getElementById("message");
-let box = document.getElementById("box");
+let para = document.getElementById("para");
 
-title.innerText = "JavaScript DOM";
 
-message.innerHTML = "<b>Hello DOM</b>";
-
-box.innerHTML = `
-    <h2>Welcome</h2>
-    <p>Learning JS</p>
-`;
+console.log(title.outerHTML);
+console.log(para.tagName);
