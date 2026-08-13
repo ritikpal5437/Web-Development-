66e4f477f65e9e7c3a1fa58c52d2694e5b754ec8
Ritik Pal <ritikpal5437@gmail.com>
2026-08-13 22:19:07 +0530

    Add events.html and update Events.js for double-click and keydown event handling

---
+++ Javascript/Events.js
@@ -0,0 +1,13 @@
+// let btn = document.getElementById("btn");
+// btn.addEventListener("click",function(e) {
+//     console.log("Button Clicked");
+// });
+let btn = document.getElementById("btn");
+btn.addEventListener("dblclick",function(e) {
+    console.log(" Double Clicked");
+});
+
+input.addEventListener("keydown",function(e) {
+    console.log("Key Down");
+}   )
+;
