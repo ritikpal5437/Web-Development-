eb79cb622034ef3905f74eca9144b1aec8d43925
Ritik Pal <ritikpal5437@gmail.com>
2026-08-15 22:53:33 +0530

    Update promise example to demonstrate rejection handling for failed login

--- Javascript/Promises/1.js
+++ Javascript/Promises/1.js
@@ -1,7 +1,15 @@
+// let prom1 = new Promise(function(resolve, reject) {
+//     resolve("login successful");
+// });
+
+// prom1.then(function(result) {
+//     console.log(result);
+// });
+
 let prom1 = new Promise(function(resolve, reject) {
-    resolve("login successful");
+    reject("login failed");
 });
 
-prom1.then(function(result) {
+prom1.catch(function(result) {
     console.log(result);
 });
