acc2fd5609c830d524638df24f8014af61642eed
Ritik Pal <ritikpal5437@gmail.com>
2026-08-15 23:03:33 +0530

    Update promise implementation to demonstrate rejection handling

--- Javascript/Promises/2.js
+++ Javascript/Promises/2.js
@@ -1,11 +1,22 @@
+// let prom1 = new Promise(function (resolve,reject) {
+    
+//      setTimeout(function() {
+//        resolve("data recived");
+       
+//     }, 2000);
+    
+// });
+// prom1.then(function(result) {
+//     console.log(result);
+// });
 let prom1 = new Promise(function (resolve,reject) {
     
      setTimeout(function() {
-       resolve("data recived");
+       reject("data is not recived");
        
     }, 2000);
     
 });
-prom1.then(function(result) {
+prom1.catch(function(result) {
     console.log(result);
 });
