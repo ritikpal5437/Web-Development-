f6d4b4240feb0084162f72e3a76f19ba8c7970b0
Ritik Pal <ritikpal5437@gmail.com>
2026-08-16 22:02:54 +0530

    Update data function to handle server error rejection

--- Javascript/Async/Await.js/async.js
+++ Javascript/Async/Await.js/async.js
@@ -1,9 +1,28 @@
+// async function data() {
+//     return new Promise((resolve, reject) => {
+//         resolve("data recived");
+//         setTimeout(() => {  
+            
+//         }, 2000);
+//     });
+// }
+
+// async function main() {
+//     try {
+//         let result = await data();
+//         console.log(result);
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+// main();
 async function data() {
     return new Promise((resolve, reject) => {
-        
+       
         setTimeout(() => {  
-            resolve("data recived");
-        }, 2000);
+             reject("server error");
+        }, 3000);
     });
 }
 
