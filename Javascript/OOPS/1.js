54d62f9b27517f1c93aeb8291d1ee273a7866c92
Ritik Pal <ritikpal5437@gmail.com>
2026-08-17 22:24:42 +0530

    Add student class with constructor and instance creation

---
+++ Javascript/OOPS/1.js
@@ -0,0 +1,10 @@
+class student {
+    constructor(name, age, course) {
+        this.name = name;
+        this.age = age;
+        this.course = course;
+    }
+}
+
+let student1 = new student("ritik", 21, "mca");
+console.log(student1);
