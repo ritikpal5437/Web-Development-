90e9d8906783e82cb2ac554015d727bfb20687dd
Ritik Pal <ritikpal5437@gmail.com>
2026-08-17 22:29:19 +0530

    Add introduce method to student class and create new methd.js file

--- Javascript/OOPS/1.js
+++ Javascript/OOPS/1.js
@@ -4,7 +4,11 @@ class student {
         this.age = age;
         this.course = course;
     }
-}
 
+introduce(){
+console.log("Hi, my name is Ritik")
+}
+}
 let student1 = new student("ritik", 21, "mca");
 console.log(student1);
+student1.introduce()
