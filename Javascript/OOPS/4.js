a459463db3725f4bc7c8052c9b0f53202db31dc6
Ritik Pal <ritikpal5437@gmail.com>
2026-08-17 22:57:28 +0530

    super

--- Javascript/OOPS/4.js
+++ Javascript/OOPS/4.js
@@ -8,10 +8,21 @@ class Animal{
     }
 }
     class Dog extends Animal{
+        constructor(name,breed){
+            super(name);
+            this.breed=breed;
+        }
         speak(){
             console.log("dog barks");
             
         }
+        showdetails(){
+            console.log("name:",this.name);
+            console.log("breed:",this.breed);
+            
+        }
     }
-dog1 = new Dog("Tommy")
-dog1.speak()
+
+
+dog1 = new Dog("Tommy", "Labrador")
+dog1.showdetails()
