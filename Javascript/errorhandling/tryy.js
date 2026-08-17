b996496ea322bdd769e9399c9a91471b7fe3469c
Ritik Pal <ritikpal5437@gmail.com>
2026-08-17 15:16:03 +0530

    Refactor input handling and error checking in tryy.js

--- Javascript/errorhandling/tryy.js
+++ Javascript/errorhandling/tryy.js
@@ -1,7 +1,33 @@
-let a =prompt  ("enter 1 no.")
-let b =prompt ("enter 2 no.")
-try{let sum = parseInt(a)+parseInt(b)
+// let a =prompt  ("enter 1 no.")
+// let b =prompt ("enter 2 no.")
+// try{let sum = parseInt(a)+parseInt(b)
 
-} catch(error) {
-    console.error("An error occurred:", error);
-}console.log("sum is",sum)
+// } catch(error) {
+//     console.error("An error occurred:", error);
+// }console.log("sum is",sum)
+let a = prompt("Enter first number")
+
+let b = prompt("Enter second number")
+if (isNaN(a) || isNaN(b)) {
+    throw SyntaxError("Sorry this is not allowed")
+}
+
+let sum = parseInt(a) + parseInt(b)
+
+function main(){ 
+    let x = 1;
+    try {
+        console.log("The sum is ", sum * x)
+        return true
+        
+    } catch (error) {
+        console.log("Error ")
+        return false
+    } 
+    finally{
+        console.log("files are being closed and db connection is being closed")
+    }
+  
+}
+
+let c = main()
