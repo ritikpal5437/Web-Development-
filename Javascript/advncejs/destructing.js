4a439782887ae6e728a237e3dcac726f590adc9f
Ritik Pal <ritikpal5437@gmail.com>
2026-08-18 22:34:50 +0530

    Add destructuring examples for arrays in JavaScript

---
+++ Javascript/advncejs/destructing.js
@@ -0,0 +1,20 @@
+let fruits = ["Apple", "Banana", "Mango"];
+let fruit1 = fruits[0];
+let fruit2 = fruits[1];
+let fruit3 = fruits[2];
+
+console.log(fruit1);
+console.log(fruit2);
+console.log(fruit3);
+
+// Array ke elements ko directly alag-alag variables me unpack karna.
+
+let numbers = [10, 20, 30];
+let a = numbers[0];
+let b = numbers[1];
+let c = numbers[2];
+
+console.log(a);
+console.log(b);
+console.log(c);
+
