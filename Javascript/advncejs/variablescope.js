43a659fb5321439031b8dc5b1a8099add7277c40
Ritik Pal <ritikpal5437@gmail.com>
2026-08-18 23:36:27 +0530

    everthing from scratch by me

--- Javascript/advncejs/variablescope.js
+++ Javascript/advncejs/variablescope.js
@@ -12,13 +12,3 @@ function showname() {
 }
 showname();
 // here name variable is in global scope, so it can be accessed inside the function showname().
-let globalName = "ritik";
-
-function showname() {
-    let localName = "vicky";
-    console.log(localName);
-}
-
-// showname();
-console.log(globalName);
-
