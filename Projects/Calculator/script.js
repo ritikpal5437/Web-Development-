aa91a155124b746a3657a9eb279ffc25d79462ae
Ritik Pal <ritikpal5437@gmail.com>
2026-08-20 16:19:13 +0530

    Implement click functionality for calculator buttons: update display logic to handle initial zero state

--- Projects/Calculator/script.js
+++ Projects/Calculator/script.js
@@ -4,5 +4,12 @@ let button = document.querySelectorAll('.btn');
 button.forEach(function(btn){
     btn.addEventListener('click',function(){
         let value = btn.dataset.value;
+        if(display.innerText==="0"){
+            display.innerText=value;
+        }
+        else{
+        display.innerText=display.innerText+value;
+        }
     })
 });
+
