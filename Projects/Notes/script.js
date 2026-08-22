d624d0ec493ef0e67a6054d34f88250e9d91d0c9
Ritik Pal <ritikpal5437@gmail.com>
2026-08-22 15:51:30 +0530

    Implement note creation functionality with dynamic input fields

---
+++ Projects/Notes/script.js
@@ -0,0 +1,12 @@
+let button = document.querySelector("button");
+let notesContainer = document.querySelector(".notes-container");
+
+button.addEventListener("click", function () {
+
+    let note = document.createElement("p");
+
+    note.className = "input-box";
+    note.setAttribute("contenteditable", "true");
+
+    notesContainer.appendChild(note);
+});
