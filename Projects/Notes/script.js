0c308318ee086c42751dfad1ed2dd1e9d177a079
Ritik Pal <ritikpal5437@gmail.com>
2026-08-22 15:55:07 +0530

    Add delete button functionality to notes and style adjustments

--- Projects/Notes/script.js
+++ Projects/Notes/script.js
@@ -4,9 +4,16 @@ let notesContainer = document.querySelector(".notes-container");
 button.addEventListener("click", function () {
 
     let note = document.createElement("p");
-
     note.className = "input-box";
     note.setAttribute("contenteditable", "true");
 
+    let deleteBtn = document.createElement("span");
+    deleteBtn.innerHTML = "×";
+
+    note.appendChild(deleteBtn);
     notesContainer.appendChild(note);
+
+    deleteBtn.addEventListener("click", function () {
+        note.remove();
+    });
 });
