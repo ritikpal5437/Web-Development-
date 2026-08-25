8a17ef04d3c2ce937e1e5c9b1a8c04570fc5b546
Ritik Pal <ritikpal5437@gmail.com>
2026-08-25 22:28:04 +0530

    Refactor quote fetching and tweeting functionality into separate JavaScript file

--- Projects/Quote/qu.js
+++ Projects/Quote/qu.js
@@ -1,2 +1,21 @@
 //  https://github.com/lukePeavey/quotable api for quotes
 
+ 
+        const quote = document.getElementById("quote")
+        const author = document.getElementById("author")
+        const api_url="https://dummyjson.com/quotes/random";
+        async function getquote(url){
+            const response = await fetch(url);
+            var data = await response.json();
+            // console.log(data);
+            quote.innerHTML=data.quote;
+            author.innerHTML=data.author;
+
+            
+        }
+        getquote(api_url);
+        function tweet() {
+            const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(quote.innerText + " " + author.innerText)}`;
+            window.open(tweetUrl, "Tweet Window", "width=600,height=400");
+        }
+    
